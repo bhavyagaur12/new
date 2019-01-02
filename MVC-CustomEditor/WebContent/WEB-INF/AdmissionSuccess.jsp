b67124bb7839eb8b3f0c26b045cef91608bfa2c1
PRIYANSHU <PRIYANSHU@DESKTOP-1P809VR>
2019-01-02 20:57:09 +0530

    first

---
+++ MVC-CustomEditor/WebContent/WEB-INF/AdmissionSuccess.jsp
@@ -0,0 +1,24 @@
+
+
+<html>
+<body style="text-align: center;">
+<h1>Form is Submitted Successfully</h1>
+<br><br>
+<h4 >${message}</h4>
+<table border="3">
+<tr><td>Student Name</td><td>${student.name}</td></tr>
+<tr><td>Student Hobby</td><td>${student.hobby}</td></tr>
+<tr><td>Student Mobile Number</td><td>${student.mobile}</td></tr>
+<tr><td>Student skills</td><td>${student.skills}</td></tr>
+<tr><td>Student Street</td><td>${student.address.street}</td></tr>
+<tr><td>Student City</td><td>${student.address.city}</td></tr>
+<tr><td>Student Country</td><td>${student.address.country}</td></tr>
+<tr><td>Student PinCode</td><td>${student.address.pincode}</td></tr>
+
+
+
+
+</table>
+</body>
+
+</html>
---
+++ MVC-CustomEditor/src/com/spring/Student.java
@@ -0,0 +1,54 @@
+package com.spring;
+
+import java.util.ArrayList;
+
+
+
+public class Student {
+
+	private String name;
+	private String hobby;
+	private Long mobile;
+//	private Date date;
+	private ArrayList<String> skills;
+	private Address address;
+
+	
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public Long getMobile() {
+		return mobile;
+	}
+	public void setMobile(Long mobile) {
+		this.mobile = mobile;
+	}
+//	public Date getDate() {
+//		return date;
+//	}
+//	public void setDate(Date date) {
+//		this.date = date;
+//	}
+	public ArrayList<String> getSkills() {
+		return skills;
+	}
+	public void setSkills(ArrayList<String> skills) {
+		this.skills = skills;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHobby() {
+		return hobby;
+	}
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+	
+}
